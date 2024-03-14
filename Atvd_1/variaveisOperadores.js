13d3322436001f0e12f3d70518f17e1306f9cb50
CutrimOff <129192824+CutrimOff@users.noreply.github.com>
2024-03-14 00:49:54 -0300

    Add files via upload
    Caio Alexandre Cutrim Medeiros - 5° Semestre - 2024 - UCB

---
+++ Atvd_1/variaveisOperadores.js
@@ -0,0 +1,19 @@
+let a = 10;
+let b = 10;
+
+
+let op="+";
+let r = a + b;
+console.log(`o resultado da ${op} entre ${a} e ${b} é: ${r}`);
+
+op="-";
+r = a - b;
+console.log(`o resultado da ${op} entre ${a} e ${b} é: ${r}`);
+
+op="*";
+r = a * b;
+console.log(`o resultado da ${op} entre ${a} e ${b} é: ${r}`);
+
+op="/";
+r = a / b;
+console.log(`o resultado da ${op} entre ${a} e ${b} é: ${r}`);
